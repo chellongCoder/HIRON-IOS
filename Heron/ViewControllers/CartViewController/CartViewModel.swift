c7f9f1fa198b5cc7b9c912d7ab00095323ff1811
TTNguyen <nttriet9a4@gmail.com>
2022-05-19 16:19:26 +0700

    Checkout

--- Heron/ViewControllers/CartViewController/CartViewModel.swift
+++ Heron/ViewControllers/CartViewController/CartViewModel.swift
@@ -11,6 +11,27 @@ class CartViewModel: NSObject {
     weak var controller     : CartViewController? = nil
     var cartDataSource      : CartDataSource? = nil
     
+    func checkout() {
+        self.controller?.startLoadingAnimation()
+        assert(cartDataSource != nil, "Cart empty")
+        _AppDataHandler.checkout(cart: self.cartDataSource!) { errorMessage, successMessage in
+            self.controller?.endLoadingAnimation()
+            
+            if errorMessage != nil {
+                let alertVC = UIAlertController.init(title: NSLocalizedString("Error", comment: ""), message: errorMessage, preferredStyle: .alert)
+                alertVC.addAction(UIAlertAction.init(title: NSLocalizedString("OK", comment: ""), style: .default, handler: { action in
+                    alertVC.dismiss()
+                }))
+                _NavController.showAlert(alertVC)
+                return
+            }
+            
+            //TODO: Clear cart
+            self.reloadCart()
+        }
+        
+    }
+    
     func addToCart(product: ProductDataSource, quantity: Int? = 1) {
         self.controller?.startLoadingAnimation()
         _AppDataHandler.addToCart(listProducts: [product]) { errorMessage, successMessage in
