ef10f74b3159aa1e8975e81eb56b4188985511e0
TTNguyenDev <nttriet9a4@gmail.com>
2022-06-22 09:16:26 +0700

    fix conflict

---
+++ Heron/ViewControllers/BaseViewController/BaseViewController.swift
@@ -0,0 +1,41 @@
+//
+//  BaseViewController.swift
+//  Heron
+//
+//  Created by Luu Luc on 28/04/2022.
+//
+
+import UIKit
+import Alamofire
+import RxSwift
+
+class BaseViewController: UIViewController {
+    
+    internal let pageScroll     = UIScrollView()
+    let contentView             = UIView()
+    let disposeBage         = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(pageScroll)
+        pageScroll.snp.makeConstraints { (make) in
+            make.left.top.right.bottom.equalToSuperview()
+        }
+        
+        contentView.backgroundColor = .white
+        pageScroll.addSubview(contentView)
+        contentView.snp.makeConstraints { (make) in
+            make.left.top.right.bottom.width.equalToSuperview()
+        }
+        self.configUI()
+        self.bindingData()
+        self.view.layoutIfNeeded()
+        
+    }
+    
+    func configUI() {}
+    
+    func bindingData() {}
+}
