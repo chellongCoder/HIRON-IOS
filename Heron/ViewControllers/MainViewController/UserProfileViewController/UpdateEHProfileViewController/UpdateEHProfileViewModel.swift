5bedb074c5a9018804d1777b8be5286ae03067d8
Lucas Luu <luc.luu@soxes.ch>
2022-09-07 15:01:37 +0700

    Update message

--- Heron/ViewControllers/MainViewController/UserProfileViewController/UpdateEHProfileViewController/UpdateEHProfileViewModel.swift
+++ Heron/ViewControllers/MainViewController/UserProfileViewController/UpdateEHProfileViewController/UpdateEHProfileViewModel.swift
@@ -30,7 +30,23 @@ class UpdateEHProfileViewModel: NSObject {
         _EHProfileServices.updateEHProfile(ehProfile) { errorMessage, successMessage in
             self.controller?.endLoadingAnimation()
             
-            print("")
+            if errorMessage != nil {
+                let alertVC = UIAlertController.init(title: NSLocalizedString("Error", comment: ""), message: errorMessage, preferredStyle: .alert)
+                alertVC.addAction(UIAlertAction.init(title: NSLocalizedString("OK", comment: ""), style: .default, handler: { _ in
+                    alertVC.dismiss()
+                }))
+                _NavController.showAlert(alertVC)
+                return
+            }
+            
+            if successMessage != nil {
+                let alertVC = UIAlertController.init(title: NSLocalizedString("Success", comment: ""), message: successMessage, preferredStyle: .alert)
+                alertVC.addAction(UIAlertAction.init(title: NSLocalizedString("OK", comment: ""), style: .default, handler: { _ in
+                    alertVC.dismiss()
+                }))
+                _NavController.showAlert(alertVC)
+                return
+            }
         }
     }
 }
